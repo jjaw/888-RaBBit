2ada28835b9d9a2a2b28ef05fe2ab4145553a79e
Will Jaw <willjaw@wills-mbp.lan>
2021-08-30 18:26:21 -0700

    same comment as last commit

--- bit_rabbit_generation_script.py
+++ bit_rabbit_generation_script.py
@@ -1,12 +1,18 @@
 # Built with python 3, dependencies installed with pip 
 # library to generate images - Pillow 
 # https://pillow.readthedocs.io/en/stable/installation.html
-from functools import update_wrapper
 from PIL import Image
+
+import csv
+
+# library to convert rgb code to name
+from webcolors import rgb_to_name
+import webcolors
 
 # library to work with arrays 
 # https://numpy.org/
 import numpy as np
+import pandas as pd
 
 # library to interact with the operating system
 import os
@@ -24,6 +30,7 @@ import copy
 import lib.coordinates as coordinates
 from lib.randomizer import randomize_by_pixel
 from lib.rabbit_construction import create_rabbit
+import lib.trackers
 
 # gets path to be used in image creation mechanism, using os
 #
@@ -43,29 +50,28 @@ dimensions = 480, 480
 # for x in range(0-200) 
 # would generate 201 birds numbered 0-200
 
-# dictionary to track attributes of each rabbit
-rabbit_attr = {}
+tracker = {}
 
-# dictionary to track how many times each attribute appeared
-total_attr = {
-    "bg_solid": 0,
-    "bg_pixel": 0,
-    "sk_solid": 0,
-    "sk_pixel": 0,
-    "cig": 0,
-    "goggle": 0,
-    "whisker": 0,
-    "candy": 0,
-    "robot_arm": 0,
-    "jetpack": 0,
-    "angel": 0,
-    "weed": 0,
-    "diamond": 0
-}
+def render_random_image(seed, n):
+    bg_nameless, sk_nameless = 0, 0
+
+    # dictionary to track attributes of each rabbit   
+    rabbit_attr = {}
+
+    # dictionary to track how many times each attribute appeared
+    total_attr = lib.trackers.total_attr
 
 
-def render_random_image(n):
     for x in range(0, n):
+        # initialize attribute tracker for each rabbits
+        # tracking background color, skin color, and total accessories
+        rabbit_attr[x] = {
+            "bg_rgb": "",
+            "bg_color": "",
+            "sk_rgb": "",
+            "sk_color": "",
+            "acc": []
+        }
 
         # using ETH block number as starting random number seed
 
@@ -76,7 +82,17 @@ def render_random_image(n):
         # randomize bg color
         r, g, b = randint(133, 255), randint(133, 255), randint(133, 255)
         bg = (r, g, b)
+        rabbit_attr[x]["bg_rgb"] = bg
+        
+        try:
+            rabbit_attr[x]["bg_color"] = rgb_to_name(bg, spec='css3')
+        except:
+            rabbit_attr[x]["bg_color"] = "None"
 
+            #########
+            #########
+            bg_nameless += 1
+            
         # outline color
         ol = (19, 0, 0)
 
@@ -86,6 +102,15 @@ def render_random_image(n):
         # randomize skin color
         r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
         sk = (r, g, b)
+        rabbit_attr[x]["sk_rgb"] = sk
+        try:
+            rabbit_attr[x]["sk_color"] = rgb_to_name(sk, spec='css3')
+        except:
+            rabbit_attr[x]["sk_color"] = "None"
+
+            #########
+            ########
+            sk_nameless += 1
         
         # create_rabbit module creates a basic rabbit canvas for us
         basic_rabbit = create_rabbit(bg, ol, sk)
@@ -101,28 +126,32 @@ def render_random_image(n):
         if randint(0, 10000)<= 3500:
             #randomizer for skin by pixel
             randomize_by_pixel(pixels, sk)
-
+            rabbit_attr[x]["sk_color"] = "pixelated"
             # counter to track attribute count of the total collection
             total_attr["sk_pixel"] += 1
+
         else:
             total_attr["sk_solid"] += 1
+
 
         if randint(0, 10000) <= 1333:
             #randomizer for bg by pixel
             randomize_by_pixel(pixels, bg)
+            rabbit_attr[x]["bg_color"] = "pixelated"
             total_attr["bg_pixel"] += 1
+
         else:
             total_attr["bg_solid"] += 1
 
 
         # 2. check for mouth accessories
-        # Total: 45.00%
-        # weed: 33.33% * 45.00%
-        # cig: 15.00% * 45.00%
-        # candy: 51.67% * 45.00%
+        # Total: 55.55%
+        # weed: 33.33% * 55.55%
+        # cig: 15.00% * 55.5%
+        # candy: 51.67% * 55.55%
 
-        mouth_prob = 5500
-        if randint(0, 10000) > mouth_prob:
+        mouth_prob = 5555
+        if randint(0, 10000) <= mouth_prob:
             mouth_attr = True
         try:
             if mouth_attr:
@@ -131,29 +160,44 @@ def render_random_image(n):
                 if mouth_accessory <= 3333:
                     # draw weed
                     coordinates.weed_draw(pixels)
+                    rabbit_attr[x]["acc"].append("weed")
                     total_attr["weed"] += 1
                     mouth_attr = False
                 elif mouth_accessory >= 8500:
-                    # draw candy
-                    coordinates.cig_draw(pixels)
-                    total_attr["cig"] += 1
+                    # draw cig
+                    burn_color = coordinates.cig_draw(pixels)
+                    rabbit_attr[x]["acc"].append("cig"+ burn_color)
+
+                    # tracking cig by burn color
+                    if burn_color == "_red_burn":
+                        total_attr["cig_red"] += 1
+                    else:
+                        total_attr["cig_blue"] +=1 
                     mouth_attr = False
                 else:
-                    coordinates.candy_draw(pixels)
-                    total_attr["candy"] += 1
+                    # draw candy
+                    candy_color = coordinates.candy_draw(pixels)
+                    rabbit_attr[x]["acc"].append("candy" + candy_color)
+
+                    # tracking cig by burn color
+                    if candy_color == "_red":
+                        total_attr["candy_red"] += 1
+                    else:
+                        total_attr["candy_green"] +=1 
+                    
                     mouth_attr = False
         except:
             print("No mouth accessory")
 
         # 3. check for robot arm
         # 
-        # Robot Arm: 12.88%
+        # Robot Arm: 15.88%
         #
 
         # Requires special interaction with the back acccessories:
         # Will render robot arm after the back accessories check
 
-        robot_arm_prob = 1288
+        robot_arm_prob = 1588
 
         if randint(0, 10000) <= robot_arm_prob:
             # Variable to track whehter model has robot arm
@@ -167,7 +211,7 @@ def render_random_image(n):
         # 4. check for jetback or angel
         # Totaly: 18.88%
         # Angel_Wing: 76.67% * 18.88%
-        # Jet_Pack: 23.33% 18.88%
+        # Jet_Pack: 23.33% * 18.88%
 
         back_prob = 1888
 
@@ -183,6 +227,8 @@ def render_random_image(n):
                 if back_accessory <= jetpack_prob and robot_arm_attr:
                     coordinates.robot_arm_draw(pixels)
                     coordinates.jetpack_draw(pixels)
+                    rabbit_attr[x]["acc"].append("robot_arm")
+                    rabbit_attr[x]["acc"].append("jetpack")
                     total_attr["robot_arm"] += 1
                     total_attr["jetpack"] += 1
                     # set back attributes back to false after it's drawn
@@ -197,6 +243,8 @@ def render_random_image(n):
                 elif back_accessory > jetpack_prob and robot_arm_attr: 
                     coordinates.angel_draw(pixels)
                     coordinates.robot_arm_draw(pixels)
+                    rabbit_attr[x]["acc"].append("angel")
+                    rabbit_attr[x]["acc"].append("robot_arm")
                     total_attr["angel"] += 1
                     total_attr["robot_arm"] += 1
                     back_attr = False
@@ -205,11 +253,13 @@ def render_random_image(n):
 
                 elif back_accessory <= jetpack_prob:
                     coordinates.jetpack_draw(pixels)
+                    rabbit_attr[x]["acc"].append("jetpack")
                     total_attr["jetpack"] += 1
                     back_attr = False
                 
                 elif back_accessory > jetpack_prob:
                     coordinates.angel_draw(pixels)
+                    rabbit_attr[x]["acc"].append("angel")
                     total_attr["angel"] += 1
                     back_attr = False
         
@@ -221,6 +271,7 @@ def render_random_image(n):
             # check whether the robot arm has been drawn
             if robot_arm_attr and not robot_drawn:
                 coordinates.robot_arm_draw(pixels)
+                rabbit_attr[x]["acc"].append("robot_arm")
                 total_attr["robot_arm"] += 1
                 # set arm attribute back after it's drawn
                 robot_arm_attr = False
@@ -235,24 +286,52 @@ def render_random_image(n):
 
         # 5. check for whisker
         # 
-        # Whisker: 5.88%
+        # Whisker: 8.88%
         #
-        whisker_prob = 588
+        whisker_prob = 888
         
         if randint(0, 10000) < whisker_prob:
             whisker_attr = True
         try:
             if whisker_attr:
                 coordinates.whisker_draw(pixels)
+                rabbit_attr[x]["acc"].append("whisker")
                 total_attr["whisker"] += 1
                 whisker_attr = False
         except:
             print("Not a wise rabbit")
 
+        # 6. check for goggle
+        #
+        # Goggle: 31.37
+        #
 
+        goggle_prob = 3137
+        
+        if randint(0, 10000) <= goggle_prob:
+            goggle_attr = True
+        try:
+            if goggle_attr:
+                lens_color = coordinates.goggle_draw(pixels)
 
+                # tracking goggle by lens color
+                if lens_color == "_cyan_lens":
+                    total_attr["goggle_cyan"] += 1
+                elif lens_color == "_yellow_lens":
+                    total_attr["goggle_yellow"] += 1
+                elif lens_color == "_magenta_lens":
+                    total_attr["goggle_magenta"] += 1 
+                else:
+                    total_attr["goggle_white"] += 1
 
-        # 6. check for diamond
+                rabbit_attr[x]["acc"].append("goggle" + lens_color)
+
+                goggle_attr = False
+        except:
+            print("Does not have a goggle")
+            goggle_attr = False
+
+        # 7. check for diamond
         # 
         # Diamond: 2.88%
         #
@@ -264,30 +343,20 @@ def render_random_image(n):
         try:
             if diamond_attr:
                 coordinates.diamond_draw(pixels)
+                rabbit_attr[x]["acc"].append("diamond")
                 total_attr["diamond"] += 1
                 diamond_attr = False
         except:
             print("Paper Hand")
         
-        # 7. check for goggle
-        #
-        # Goggle: 31.37
-        #
 
-        goggle_prob = 3137
-        
-        if randint(0, 10000) <= goggle_prob:
-            goggle_attr = True
-        try:
-            if goggle_attr:
-                coordinates.goggle_draw(pixels)
-                total_attr["goggle"] += 1
-                goggle_attr = False
-        except:
-            print("Does not have a goggle")
+
+        # sort rabbit personal attributes
+        rabbit_attr[x]["acc"].sort
     
-
+        
         # convert the pixels into an array using numpy
+        
         array = np.array(pixels, dtype=np.uint8)
 
         # use PIL to create an image from the new array of pixels
@@ -295,6 +364,20 @@ def render_random_image(n):
         new_image = new_image.resize(dimensions, resample=0)
         imgname = dirname + '/rabbit_images/' + (str(x)) + '.png'
         new_image.save(imgname)
+        
 
-# select numbers of image you wish to generate
-render_random_image(50)
+    # dump total stats
+    (pd.DataFrame.from_dict(data=total_attr, orient='index')
+        .to_csv('total_stats.csv', mode="w"))
+
+    # dump rabbit individual stat
+    (pd.DataFrame.from_dict(data=rabbit_attr, orient='index')
+        .to_csv('rabbit_stats.csv', mode="w"))
+
+
+    return bg_nameless, sk_nameless
+
+# render_random_image(int seed, int num)
+# seed: for random.seed()
+# num: number of images to generate
+render_random_image(842388, 1000)
