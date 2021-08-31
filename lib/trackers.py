68daaa574b34996e8bf97b341479fbb6189fcd0a
Will Jaw <willjaw@wills-mbp.lan>
2021-08-30 18:25:22 -0700

    added colors for items, added attribute trackers for each rabbit, added attribute tracker for collection

---
+++ lib/trackers.py
@@ -0,0 +1,22 @@
+
+# variables for the total stats tracker
+total_attr = {
+    "bg_solid": 0,
+    "bg_pixel": 0,
+    "sk_solid": 0,
+    "sk_pixel": 0,
+    "cig_red": 0,
+    "cig_blue": 0,
+    "goggle_cyan": 0,
+    "goggle_yellow": 0,
+    "goggle_magenta": 0,
+    "goggle_white": 0,
+    "whisker": 0,
+    "candy_red": 0,
+    "candy_green": 0,
+    "robot_arm": 0,
+    "jetpack": 0,
+    "angel": 0,
+    "weed": 0,
+    "diamond": 0
+}
